c9cf14e427c46316b39be66558fa238b8830ffe5
Sergio Delgado <sergioalejandrods0916@hotmail.com>
2025-11-30 01:49:06 -0500

    feat: Implement Pokemon card refactor, add new design and implement new project configuration

---
+++ Constants/PokemonTypeColors.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace Acromo.Constants;
+
+public static class PokemonTypeColors
+{
+    public const string Default = "#A0A29F";
+
+    public static readonly Dictionary<string, string> Map = new(StringComparer.OrdinalIgnoreCase)
+    {
+        { "normal", "#A0A29F" },
+        { "fire", "#F08030" },
+        { "water", "#6890F0" },
+        { "electric", "#F8D030" },
+        { "grass", "#78C850" },
+        { "ice", "#98D8D8" },
+        { "fighting", "#C03028" },
+        { "poison", "#A040A0" },
+        { "ground", "#E0C068" },
+        { "flying", "#A890F0" },
+        { "psychic", "#F85888" },
+        { "bug", "#A8B820" },
+        { "rock", "#B8A038" },
+        { "ghost", "#705898" },
+        { "dragon", "#7038F8" },
+        { "dark", "#705848" },
+        { "steel", "#B8B8D0" },
+        { "fairy", "#EE99AC" }
+    };
+}
